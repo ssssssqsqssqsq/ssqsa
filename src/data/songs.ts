0e470482dd982349377ce2f413906287488ce12b
lukafaufau <faufauluka@gmail.com>
2025-05-25 05:07:42 +0200

    Updated songs.ts

--- src/data/songs.ts
+++ src/data/songs.ts
@@ -5,7 +5,7 @@ export const songs: Song[] = [
     id: '1',
     title: 'Le song est bug, c bientôt fixe !',
     artist: 'Reload Ta Pub',
-    url: 'https://drive.google.com/file/d/1aHVi-cMYPS6l5fDOvYuxSq9VHuqw5KJ2/view?usp=sharing',
+    url: 'https://docs.google.com/uc?export=download&id=1aHVi-cMYPS6l5fDOvYuxSq9VHuqw5KJ2',
     coverArt: 'https://media.discordapp.net/attachments/1372686113826934855/1375909100017160402/ReloadFrance.png?ex=683366b4&is=68321534&hm=d7ada5e5d94e8ccd693d3eff30d21c4e0e9a7e71f8be12b85f1b47865727b594&=&format=webp&quality=lossless',
   },
 ];
