75f32fe9d1ad9b874d3d34dbee8b2edc4b0026a4
Reloadddddd <reload.contactme@gmail.com>
2025-05-25 15:00:08 +0200

    Update songs.ts

--- src/data/songs.ts
+++ src/data/songs.ts
@@ -3,7 +3,7 @@ import { Song } from '../types';
 export const songs: Song[] = [
   {
     id: '1',
-    title: 'Le song est bug, c bientôt fixe !',
+    title: 'Reload - 1 Appel, 0 Problème !',
     artist: 'Reload Ta Pub',
     url: 'https://www.youtube.com/watch?v=0ycrBHxbolE',
     coverArt: 'https://media.discordapp.net/attachments/1372686113826934855/1375909100017160402/ReloadFrance.png?ex=683366b4&is=68321534&hm=d7ada5e5d94e8ccd693d3eff30d21c4e0e9a7e71f8be12b85f1b47865727b594&=&format=webp&quality=lossless',
